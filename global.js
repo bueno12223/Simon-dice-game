0cd209e05cb27b754d35decff11cc54305821f66
Jesus <berriojesus122@gmail.com>
2020-09-25 11:47:23 -0400

    se genera y se ilumina la secuencia en aleatorio

--- global.js
+++ global.js
@@ -8,9 +8,11 @@ class juego {
    constructor(){
       this.iniciar()
       this.generarSecuencia()
+      this.siguienteNivel()
    }
    iniciar(){
       btnEmpezar.classList.add("hide");
+      console.log("holaaa!")
       this.nivel = 1;
       this.colores = {
          celeste,
@@ -23,9 +25,39 @@ class juego {
    generarSecuencia(){
       this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4 ))
    }
+   siguienteNivel(){
+      this.iluminarSecuencia()
+   }
+   transformar(numero){
+      switch (numero){
+         case 0:
+         return "celeste"
+         case 1:
+         return "violeta"
+         case 2:
+         return "Naranja"
+         case 3:
+         return "verde"
+      }
+
+   }
+   iluminarSecuencia(){
+      for (let i = 0; i < this.nivel; i++ ){
+         const color = this.transformar(this.secuencia[i]);
+         setTimeout(() => this.iluminarColor(color), 1000 * i) } }
+   
+   
+   
+         iluminarColor(color){
+      this.colores[color].classList.add("light");
+      setTimeout(() => this.apagarColor(color), 350);
+
+   }
+   apagarColor(color){
+      this.colores[color].classList.remove("light");
+   }
 
 }
 function empezarJuego (){   
 
 window.juego = new juego(); } 
-var x
